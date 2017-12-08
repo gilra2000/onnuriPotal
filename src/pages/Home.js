888704bcb79ee749922bb313c92232e24ac393cd
gilra2000 <>
2017-12-08 13:50:15 +0900

    배너 버그 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,19 +7,32 @@ const cx = classNames.bind(styles);
 
 class Home extends React.Component {
 
-    state = {
-        slideIndex: 0
-    }
+  constructor(props){
+    super(props);
 
-    render(){
-      return (
-        <Carousel autoplay={true} autoplayInterval={3000} slideIndex={this.state.slideIndex} wrapAround={true}>
+    this.state = {
+      slideIndex:0
+    }
+  }
+
+  componentDidMount(){
+    // console.log(this.refs.carousel)
+    setTimeout(()=>{
+      this.setState({slideIndex: 0})
+    }, 10)
+  }
+
+  render(){
+    return (
+      <div style={{margin:'auto'}}>
+        <Carousel ref="carousel" slideIndex={this.state.slideIndex} wrapAround={true}>
           <img src="http://www.onnuri.org/wp-content/uploads/2017/12/2017_petitM.jpg"/>
           <img src="http://www.onnuri.org/wp-content/uploads/2017/11/angeltree_17.png"/>
           <img src="http://www.onnuri.org/wp-content/uploads/2017/02/2017_slogan_mainIndex06-1.png"/>
         </Carousel>
-      )
-    }
+      </div>
+    )
+  }
 }
 
 export default Home;
