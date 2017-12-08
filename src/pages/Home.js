7cd7115340c513dc4c97d55f9fa8ec2bb556cad6
gilra2000 <>
2017-12-08 12:02:25 +0900

    배너 슬라이더 모듈 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import styles from '../index.scss';
 import classNames from 'classnames/bind';
+import Carousel from 'nuka-carousel';
 
 const cx = classNames.bind(styles);
 
-const Home = () => {
-    const isBlue = true;
+class Home extends React.Component {
 
-    return (
-        <div className={cx('box', {
-            blue: isBlue
-        })}>
-            <div className={cx('box-inside')}/>
-        </div>
-    )
+    state = {
+        slideIndex: 0
+    }
+
+    render(){
+      return (
+        <Carousel autoplay={true} autoplayInterval={3000} slideIndex={this.state.slideIndex} wrapAround={true}>
+          <img src="http://www.onnuri.org/wp-content/uploads/2017/12/2017_petitM.jpg"/>
+          <img src="http://www.onnuri.org/wp-content/uploads/2017/11/angeltree_17.png"/>
+          <img src="http://www.onnuri.org/wp-content/uploads/2017/02/2017_slogan_mainIndex06-1.png"/>
+        </Carousel>
+      )
+    }
 }
 
 export default Home;
