970fedd81ebbce99533a9058cecf4660c95e790d
gilra2000 <gilra200@naver.com>
2017-12-12 23:47:48 +0900

    11/13

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Menu = () => {
-    const activeStyle = {
-        color: 'greven',
-        fontSize: '2rem'
-    };
+class Menu extends React.Component {
 
-    return (
-        <div>
-            <NavLink exact to="/" activeStyle={activeStyle}>홈입니다</NavLink>
-            <NavLink exact to="/about" activeStyle={activeStyle}>About</NavLink>
-            <NavLink to="/about/foo" activeStyle={activeStyle}>About Foo</NavLink>
-            <NavLink to="/posts" activeStyle={activeStyle}>Posts</NavLink>
-            <NavLink to="/users" GactiveStyle={activeStyle}>Users</NavLink>
-        </div>
-    )
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isView: false
+        }
+    }
+
+    render(){
+        return(
+            <div>
+            <div style={{backgroundColor:'rgba(85, 85, 85, .7)', height:30}}>
+
+            </div>
+
+            <div onClick={()=>{this.setState({isView: !this.state.isView})}} style={{height:54, backgroundColor:'#f6f6f6'}}>
+
+            </div>
+                {
+                    this.state.isView &&
+                    <div style={{height:100, backgroundColor:'red', float:'top'}}></div>
+                }
+            </div>
+
+        )
+    }
+
 }
 
 export default Menu;
+
+{/*<NavLink exact to="/" >홈입니다</NavLink>*/}
+{/*<NavLink exact to="/about" >About</NavLink>*/}
+{/*<NavLink to="/about/foo" >About Foo</NavLink>*/}
+{/*<NavLink to="/posts" >Posts</NavLink>*/}
+{/*<NavLink to="/users" >Users</NavLink>*/}
