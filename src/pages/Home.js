4217ce941936f5c6a05f58688c9120cc2cea0404
gilra2000 <gilra200@naver.com>
2017-12-20 07:37:44 +0900

    메뉴수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,7 +26,6 @@ class Home extends React.Component {
     }
   }
 
-
     toggleDrawer = (side, open) => () => {
         this.setState({
             [side]: open,
@@ -34,6 +33,12 @@ class Home extends React.Component {
     };
 
   componentDidMount(){
+      setTimeout(()=>{
+          this.setState({
+              slideIndex:0
+          })
+      }, 10)
+
   }
 
   render(){
@@ -50,7 +55,7 @@ class Home extends React.Component {
           <div>
               {/*<Button onClick={this.toggleDrawer('left', true)}>Open Left</Button>*/}
 
-              <div style={{backgroundColor:'#f5f5f5'}}>
+              <div style={{backgroundColor:'#f5f5f5', height:544}}>
                   <Carousel ref="carousel" autoplay={true} autoplayInterval={5000} slideIndex={this.state.slideIndex} wrapAround={true} width={'100%'} style={{maxWidth:1180, minWidth:400, margin:'auto'}} >
                       <img src="http://www.onnuri.org/wp-content/uploads/2017/12/2017_petitM.jpg"/>
                       <img src="http://www.onnuri.org/wp-content/uploads/2017/11/angeltree_17.png"/>
